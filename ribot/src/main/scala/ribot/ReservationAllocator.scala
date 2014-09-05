85dec35bf0d241bffbbc3675cfbdef38b5ff03de
Stuart Boundy <stuart.boundy@guardian.co.uk>
2014-09-05 22:47:39 +0100

    Addressing issue #2 - spending points remainder

--- ribot/src/main/scala/ribot/ReservationAllocator.scala
+++ ribot/src/main/scala/ribot/ReservationAllocator.scala
@@ -17,14 +17,14 @@ object ReservationAllocator {
           groupWithPotentialSparePoint.spendSpare(unallocatedDesiredReservations.last))
   }
 
-    def possiblyApply(groups: List[ReservationGroup], r: ReservationCriteria): List[ReservationGroup] = {
-      def possiblyApplyImpl(nextGroups: List[ReservationGroup]): List[ReservationGroup] = {
-        nextGroups match {
-          case Nil => Nil
-          case head :: rest if head.sparePoints >= r.points => head.spend(r) :: rest
-          case head :: rest => head :: possiblyApplyImpl(rest)
-        }
+  def possiblyApply(groups: List[ReservationGroup], r: ReservationCriteria): List[ReservationGroup] = {
+    def possiblyApplyImpl(nextGroups: List[ReservationGroup]): List[ReservationGroup] = {
+      nextGroups match {
+        case Nil => Nil
+        case head :: rest if head.sparePoints >= r.points => head.spend(r) :: rest
+        case head :: rest => head :: possiblyApplyImpl(rest)
       }
+    }
 
     possiblyApplyImpl(groups)
   }
